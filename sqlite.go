c30e7b84ac28f12d5f579f69265328ee53c64a14
kazyk <kazrbb@gmail.com>
2012-08-21 02:16:34 +0900

    add src

---
+++ sqlite.go
@@ -0,0 +1,216 @@
+package sqlite
+
+/*
+#cgo LDFLAGS: -lsqlite3
+#include <sqlite3.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int exec(sqlite3 *db, const char *sql) {
+	return sqlite3_exec(db, sql, NULL, NULL, NULL);
+}
+int bind_text(sqlite3_stmt *stmt, int i, const char *str, int n) {
+	return sqlite3_bind_text(stmt, i, str, n, SQLITE_TRANSIENT);
+}
+*/
+import "C"
+import (
+	"database/sql"
+	"database/sql/driver"
+	"io"
+	"errors"
+	"unsafe"
+)
+
+func init() {
+	sql.Register("sqlite", Driver{})
+}
+
+type Driver struct {
+}
+
+func (Driver) Open(name string) (driver.Conn, error) {
+	n := C.CString(name)
+	defer C.free(unsafe.Pointer(n))
+
+	var conn Conn
+	r := C.sqlite3_open(n, &conn.db)
+	if r != C.SQLITE_OK {
+		return &conn, driver.ErrBadConn
+	}
+	return &conn, nil
+}
+
+type Conn struct {
+	db *C.sqlite3
+}
+
+func (conn Conn) Prepare(query string) (driver.Stmt, error) {
+	s := C.CString(query)
+	defer C.free(unsafe.Pointer(s))
+	l := C.int(C.strlen(s))
+
+	var stmt Stmt
+	var tail *C.char
+	r := C.sqlite3_prepare_v2(conn.db, s, l, &stmt.stmt, &tail)
+
+	if r != C.SQLITE_OK {
+		return &stmt, driver.ErrBadConn
+	}
+	return &stmt, nil
+}
+
+func (conn Conn) Close() error {
+	r := C.sqlite3_close(conn.db)
+	if r == C.SQLITE_OK {
+		return driver.ErrBadConn
+	}
+	return nil
+}
+
+func (conn Conn) Begin() (driver.Tx, error) {
+	panic("not supported yet")
+}
+
+type Stmt struct {
+	stmt *C.sqlite3_stmt
+}
+
+func (stmt Stmt) Close() error {
+	r := C.sqlite3_finalize(stmt.stmt)
+	if r != C.SQLITE_OK {
+		return driver.ErrBadConn
+	}
+	return nil
+}
+
+func (stmt Stmt) NumInput() int {
+	return int(C.sqlite3_bind_parameter_count(stmt.stmt))
+}
+
+func (stmt Stmt) bind(args []driver.Value) error {
+	if C.sqlite3_clear_bindings(stmt.stmt) != C.SQLITE_OK {
+		return driver.ErrBadConn
+	}
+	for i, arg := range args {
+		r := C.int(-1)
+		switch val := arg.(type) {
+		case int64:
+			r = C.sqlite3_bind_int64(stmt.stmt, C.int(i+1), C.sqlite3_int64(val))
+		case float64:
+			r = C.sqlite3_bind_double(stmt.stmt, C.int(i+1), C.double(val))
+		case bool:
+			if val {
+				r = C.sqlite3_bind_int64(stmt.stmt, C.int(i+1), C.sqlite3_int64(1))
+			} else {
+				r = C.sqlite3_bind_int64(stmt.stmt, C.int(i+1), C.sqlite3_int64(0))
+			}
+		case nil:
+			r = C.sqlite3_bind_null(stmt.stmt, C.int(i+1))
+		case string:
+			str := C.CString(val)
+			defer C.free(unsafe.Pointer(str))
+			l := C.int(C.strlen(str))
+			r = C.bind_text(stmt.stmt, C.int(i+1), str, l)
+		default:
+			panic("unsupported type")
+		}
+
+		if r != C.SQLITE_OK {
+			return driver.ErrBadConn
+		}
+	}
+	return nil
+}
+
+func (stmt Stmt) Exec(args []driver.Value) (driver.Result, error) {
+	if err := stmt.bind(args); err != nil {
+		return nil, err
+	}
+
+	r := C.sqlite3_step(stmt.stmt)
+	db := C.sqlite3_db_handle(stmt.stmt)
+
+	if r != C.SQLITE_DONE && r != C.SQLITE_ROW {
+		return nil, errors.New(C.GoString(C.sqlite3_errmsg(db)))
+	}
+
+	var result Result
+	result.rowsAffected = int64(C.sqlite3_changes(db))
+	result.lastInsertId = int64(C.sqlite3_last_insert_rowid(db))
+	return &result, nil
+}
+
+func (stmt Stmt) Query(args []driver.Value) (driver.Rows, error) {
+	if err := stmt.bind(args); err != nil {
+		return nil, err
+	}
+	return &Rows{stmt: stmt.stmt}, nil
+}
+
+type Result struct {
+	rowsAffected int64
+	lastInsertId int64
+}
+
+func (result Result) LastInsertId() (int64, error) {
+	return result.lastInsertId, nil
+}
+
+func (result Result) RowsAffected() (int64, error) {
+	return result.rowsAffected, nil
+}
+
+
+type Rows struct {
+	stmt *C.sqlite3_stmt
+}
+
+func (rows Rows) Columns() []string {
+	count := int(C.sqlite3_column_count(rows.stmt))
+	cols := make([]string, count)
+	for i := 0; i < count; i++ {
+		cols[i] = C.GoString(C.sqlite3_column_name(rows.stmt, C.int(i)))
+	}
+	return cols
+}
+
+func (rows Rows) Close() error {
+	if C.sqlite3_reset(rows.stmt) != C.SQLITE_OK {
+		return driver.ErrBadConn
+	}
+	return nil
+}
+
+func (rows Rows) Next(dest []driver.Value) error {
+	r := C.sqlite3_step(rows.stmt)
+	if r == C.SQLITE_DONE {
+		return io.EOF
+	}
+	if r != C.SQLITE_ROW {
+		db := C.sqlite3_db_handle(rows.stmt)
+		return errors.New(C.GoString(C.sqlite3_errmsg(db)))
+	}
+
+	count := len(dest)
+	for i := 0; i < count; i++ {
+		t := C.sqlite3_column_type(rows.stmt, C.int(i))
+		switch t {
+		case C.SQLITE_INTEGER:
+			dest[i] = int64(C.sqlite3_column_int64(rows.stmt, C.int(i)))
+		case C.SQLITE_FLOAT:
+			dest[i] = float64(C.sqlite3_column_double(rows.stmt, C.int(i)))
+		case C.SQLITE_NULL:
+			dest[i] = nil
+		case C.SQLITE_TEXT:
+			n := C.sqlite3_column_bytes(rows.stmt, C.int(i))
+			blob := C.sqlite3_column_blob(rows.stmt, C.int(i))
+			dest[i] = C.GoBytes(blob, n)
+		default:
+			panic("unsupported type")
+		}
+	}
+	return nil
+}
+
+
---
+++ sqlite_test.go
@@ -0,0 +1,78 @@
+package sqlite
+
+import (
+	"database/sql"
+	"testing"
+)
+
+func TestDriver(t *testing.T) {
+	db, err := sql.Open("sqlite", "testdb")
+	defer db.Close()
+
+	if db == nil {
+		t.Error("db is nil")
+	}
+	if err != nil {
+		t.Error("sql.Open err", err)
+	}
+
+	result, err := db.Exec("create table testtable (id integer primary key, name text);")
+	if result == nil {
+		t.Error("result is nil")
+	}
+	if err != nil {
+		t.Error("db.Exec err", err)
+	}
+
+	defer db.Exec("drop table testtable;")
+
+	const n = 3
+	for i := 0; i < n; i++ {
+		result, err = db.Exec("insert into testtable (id, name) values (?, ?);", i * 100, "hogehoge")
+		if result == nil {
+			t.Error("result is nil")
+		}
+		nrows, err := result.RowsAffected()
+		if nrows != 1 {
+			t.Error("invalid rows affected", nrows)
+		}
+		if err != nil {
+			t.Error("db.Exec err", err)
+		}
+	}
+
+	rows, err := db.Query("select id, name from testtable order by id asc;")
+	if rows == nil {
+		t.Error("rows is nil")
+	}
+	if err != nil {
+		t.Error("db.Query err", err)
+	}
+	defer rows.Close()
+
+	cols, err := rows.Columns()
+	if len(cols) != 2 {
+		t.Error("cols")
+	}
+	if cols[0] != "id" || cols[1] != "name" {
+		t.Error("invalid columns", cols)
+	}
+
+	i := 0
+	for rows.Next() {
+		var id int
+		var name string
+		rows.Scan(&id, &name)
+		if id != i * 100 {
+			t.Error("invalid id value", id)
+		}
+		if name != "hogehoge" {
+			t.Error("invalid name value", name)
+		}
+		i++
+	}
+
+	if i != n {
+		t.Error()
+	}
+}
